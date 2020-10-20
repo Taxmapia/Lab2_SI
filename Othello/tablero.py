c5885dceb2b818cf482b9bf5eba53a9da1dc1e56
Taxmapia <46612964+Taxmapia@users.noreply.github.com>
2020-10-19 21:04:40 -0300

    Tablero

--- Othello/tablero.py
+++
@@ -1,9 +0,0 @@
-from .constantes import BLANCO , NEGRO , FILAS , BLOQUE_SIZE , pygame
-class Tablero:
-    def __init__(self):
-        self.fichas_rojas = self.fichas_azules = 2 
-    def Dibujar_Cuadro(self,win):
-        win.fill(NEGRO)
-        for fila in range(FILAS):
-            for col in range(fila % 2, FILAS , 2):
-                pygame.draw.rect(win,BLANCO,(fila*BLOQUE_SIZE,col*BLOQUE_SIZE,BLOQUE_SIZE,BLOQUE_SIZE))
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
+import os
 from Othello.constantes import *
-from Othello.tablero import Tablero
-import Othello.ficha
 
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('El Mejor Othello Del Mundo')
 
+tab_list = pygame.sprite.Group()
 f_list = pygame.sprite.Group()
 
+class Casilla(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("casilla.png").convert()
+        self.image.set_colorkey(BLANCO)
+        self.rect = self.image.get_rect()
+
+
+def Llenar_Tablero():
+    WIN.fill(BLANCO)
+    for fila in range(FILAS):
+        for col in range(COLUMNAS):
+            casilla = Casilla()
+            casilla.rect.x = (fila * BLOQUE_SIZE)
+            casilla.rect.y = (col * BLOQUE_SIZE)
+            tab_list.add(casilla)
 def main():
     run = True
     clock = pygame.time.Clock()
-    tablero = Tablero()
     while run:
         clock.tick(FPS)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        tablero.Dibujar_Cuadro(WIN)
+        Llenar_Tablero()
+        tab_list.draw(WIN)
         pygame.display.update()
     pygame.quit()
 
