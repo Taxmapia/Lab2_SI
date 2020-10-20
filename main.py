c4fc82f0ff56f19f9085b9e8a16b5b7c9c159568
Taxmapia <46612964+Taxmapia@users.noreply.github.com>
2020-10-20 01:23:30 -0300

    Pa que el pancho deje de wear

--- main.py
+++ main.py
@@ -11,16 +11,37 @@ pygame.display.set_caption('El Mejor Othello Del Mundo')
 tab_list = pygame.sprite.Group()
 f_list = pygame.sprite.Group()
 
+class Ficha(pygame.sprite.Sprite):
+    def __init__(self, color):
+        super().__init__()
+        if color == Constantes.R:
+            self.color = Constantes.R
+            self.ficha_roja = Constantes.F_R.convert()
+            self.ficha_roja.set_colorkey(Constantes.BLANCO)
+            self.rect = self.ficha_rojo.get_rect()
+        else:
+            self.color = Constantes.B
+            self.ficha_azul =  Constantes.F_A.convert()
+            self.ficha_azul.set_colorkey(Constantes.BLANCO)
+            self.rect = self.ficha_azul.get_rect()
+        self.x , self.y = 0 , 0
+        
+        
+
+    def Color(self):
+        print(self.color)
+
 class Casilla(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = Constantes.CASILLA.convert()
-        self.image.set_colorkey(Constantes.BLANCO)
+        self.image = Constantes.CASILLA
         self.rect = self.image.get_rect()
+    def Calc_pos(self):
+        print()
 
 
 def Llenar_Tablero():
-    WIN.fill(Constantes.BLANCO)
+    WIN.fill(Constantes.NEGRO)
     for fila in range(Constantes.FILAS):
         for col in range(Constantes.COLUMNAS):
             casilla = Casilla()
@@ -28,6 +49,7 @@ def Llenar_Tablero():
             casilla.rect.y = (col * Constantes.BLOQUE_SIZE)
             tab_list.add(casilla)
 def main():
+    print(Constantes.BLOQUE_SIZE)
     run = True
     clock = pygame.time.Clock()
     while run:
@@ -38,6 +60,7 @@ def main():
                 run = False
         Llenar_Tablero()
         tab_list.draw(WIN)
+
         pygame.display.update()
     pygame.quit()
 
