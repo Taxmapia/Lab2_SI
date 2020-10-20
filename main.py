7e3d1a0fafa6f9e8fb0a9f68289045abd8528dab
Taxmapia <46612964+Taxmapia@users.noreply.github.com>
2020-10-19 21:49:53 -0300

    Tablero Mejorado

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-from Othello.constantes import *
+import Constantes 
+import pygame
 
-FPS = 60
+FPS = 30
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+WIN = pygame.display.set_mode((Constantes.WIDTH, Constantes.HEIGHT))
 pygame.display.set_caption('El Mejor Othello Del Mundo')
 
 tab_list = pygame.sprite.Group()
@@ -12,18 +14,18 @@ f_list = pygame.sprite.Group()
 class Casilla(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("casilla.png").convert()
-        self.image.set_colorkey(BLANCO)
+        self.image = Constantes.CASILLA.convert()
+        self.image.set_colorkey(Constantes.BLANCO)
         self.rect = self.image.get_rect()
 
 
 def Llenar_Tablero():
-    WIN.fill(BLANCO)
-    for fila in range(FILAS):
-        for col in range(COLUMNAS):
+    WIN.fill(Constantes.BLANCO)
+    for fila in range(Constantes.FILAS):
+        for col in range(Constantes.COLUMNAS):
             casilla = Casilla()
-            casilla.rect.x = (fila * BLOQUE_SIZE)
-            casilla.rect.y = (col * BLOQUE_SIZE)
+            casilla.rect.x = (fila * Constantes.BLOQUE_SIZE)
+            casilla.rect.y = (col * Constantes.BLOQUE_SIZE)
             tab_list.add(casilla)
 def main():
     run = True
