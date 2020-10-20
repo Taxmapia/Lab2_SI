cd7ab92e0cac45368d4ce0b3e35c167b7ec8652d
LeBaishompu <f.sezaballay@uandresbello.edu>
2020-10-20 02:38:29 -0300

    20/10/2020 2.38 am logramos poner azul y rojo al medio

--- main.py
+++ main.py
@@ -1,25 +1,66 @@
-from Othello.constantes import *
-from Othello.tablero import Tablero
-import Othello.ficha
+import Constantes 
+import pygame
 
-FPS = 60
+FPS = 30
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+WIN = pygame.display.set_mode((Constantes.WIDTH, Constantes.HEIGHT))
 pygame.display.set_caption('El Mejor Othello Del Mundo')
 
+tab_list = pygame.sprite.Group()
+f_list = pygame.sprite.Group()
+
+class Ficha(pygame.sprite.Sprite):
+    def __init__(self, color):
+        super().__init__()
+        if color == Constantes.R:
+            self.color = Constantes.R
+            self.ficha_roja = Constantes.F_R.convert()
+            self.ficha_roja.set_colorkey(Constantes.BLANCO)
+            self.rect = self.ficha_rojo.get_rect()
+        else:
+            self.color = Constantes.B
+            self.ficha_azul =  Constantes.F_A.convert()
+            self.ficha_azul.set_colorkey(Constantes.BLANCO)
+            self.rect = self.ficha_azul.get_rect()
+        self.x , self.y = 0 , 0
+        
+        
+
+    def Color(self):
+        print(self.color)
+
+class Casilla(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = Constantes.CASILLA
+        self.rect = self.image.get_rect()
+    def Calc_pos(self):
+        print()
+
+
+def Llenar_Tablero():
+    WIN.fill(Constantes.NEGRO)
+    for fila in range(Constantes.FILAS):
+        for col in range(Constantes.COLUMNAS):
+            casilla = Casilla()
+            casilla.rect.x = (fila * Constantes.BLOQUE_SIZE)
+            casilla.rect.y = (col * Constantes.BLOQUE_SIZE)
+            tab_list.add(casilla)
 def main():
+    print(Constantes.BLOQUE_SIZE)
     run = True
     clock = pygame.time.Clock()
-    tablero = Tablero()
     while run:
         clock.tick(FPS)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        tablero.Dibujar_Cuadro(WIN)
+        Llenar_Tablero()
+        tab_list.draw(WIN)
+
         pygame.display.update()
     pygame.quit()
 
